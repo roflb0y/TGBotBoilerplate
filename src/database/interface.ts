a108c9cb3249e8a343ec16d584095c9957aacb60
Ronbin <70452799+roflb0y@users.noreply.github.com>
2024-08-19 12:28:34 +0300

    Опять забыл

--- src/database/interface.ts
+++ src/database/interface.ts
@@ -2,8 +2,6 @@ export interface DBUser {
     userId: number,
     username: string,
     locale: string,
-    processesCount: number,
-    convertType: string,
     joinedAt: Date,
     isBanned: boolean
 }
